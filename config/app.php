d1180da48631eea4b7a3f0d03a0fdb750d66b69f
laravelkid <nguyenanhkhoa02003@gmail.com>
2023-10-28 03:05:11 +0700

    telegram_bot

--- config/app.php
+++ config/app.php
@@ -70,7 +70,8 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => env('TIMEZONE', 'Asia/Ho_Chi_Minh'),
+
 
     /*
     |--------------------------------------------------------------------------
@@ -168,6 +169,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Telegram\Bot\Laravel\TelegramServiceProvider::class,
+
     ])->toArray(),
 
     /*
@@ -183,6 +186,8 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Telegram' => Telegram\Bot\Laravel\Facades\Telegram::class,
+
     ])->toArray(),
 
 ];
